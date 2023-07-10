c0cab1d4934ee709e2ad53b9d927d62e4bdb157e
Alvaro Mewller <103400666+Mewller@users.noreply.github.com>
2023-07-10 19:43:41 -0300

    Beta-Filter
    Um filtro que puxa atestados semanalmente de uma API

--- Filter-data.py
+++ Filter-data.py
@@ -1,55 +1,80 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import PySimpleGUI as sg
 import os
+import threading
+
+
+apikey = ''
+token = ''
 
 layout = [
     [sg.Text('Data de Início:'), sg.Input(key='-DATA_INICIO-', enable_events=True), sg.CalendarButton('Selecionar', target='-DATA_INICIO-', format='%Y-%m-%d')],
     [sg.Text('  Data Final:   '), sg.Input(key='-DATA_FINAL-', enable_events=True), sg.CalendarButton('Selecionar', target='-DATA_FINAL-', format='%Y-%m-%d')],
     [sg.Text('Onde Salvar? '), sg.Input(key='-PASTA_DESTINO-', enable_events=True), sg.FolderBrowse('Selecionar')],
+    [sg.Output(size=(68, 6))],
     [sg.Button('Baixar', button_color=('white', 'Darkblue'), image_size=(50, 25), border_width=5, key='-BAIXAR-'), sg.Text('                                                                                              '),sg.Button('Sair', button_color=('Darkred'))]
+    
 ]
-sg.set_global_icon('icon.ico')
 janela = sg.Window('Filtro', layout)
 
 def baixar_anexos(data_inicio, data_final, pasta_destino):
-    global token
     try:
         request = requests.get(f"https://api.auvo.com.br/v2/login/?apiKey={apikey}&apiToken={token}")
         certo = json.loads(request.content)
         access_token = certo['result']['accessToken']
-        data = {'startDate': f'{data_inicio}T00:00:00','endDate': f'{data_final}T00:00:00'}
-        headers = {'Content-Type': 'application/json','Authorization': 'Bearer ' + access_token}
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token}
+        
+        dt_inicio = datetime.strptime(data_inicio, '%Y-%m-%d').date()
+        dt_final = datetime.strptime(data_final, '%Y-%m-%d').date()
+        
+        dt_atual = date.today() + timedelta(days=1)
+        if dt_inicio > dt_atual:
+            janela.write_event_value('-POPUP-', 'A data de início pode ser no máximo hoje.')
+            return
+        
+        if dt_final > dt_atual:
+            janela.write_event_value('-POPUP-', 'A data final pode ser no máximo hoje.')
+            return
+        
+        if dt_inicio > dt_final:
+            janela.write_event_value('-POPUP-', 'A data de início deve ser anterior ou igual à data final.')
+            return
+        
+        if dt_final - dt_inicio > timedelta(days=7):
+            janela.write_event_value('-POPUP-', 'O período selecionado não pode ser maior que 1 semana.')
+            return
+        
+        data = {'startDate': f'{data_inicio}T00:00:00', 'endDate': f'{data_final}T23:59:59'}
         page = 1
         pageSize = 100
         order = 'asc'
         selectfields = ''
-        numero_solicitacao = 0 
+        numero_solicitacao = 0
+        
         while True:
             if page != 100:
-                numero_solicitacao = numero_solicitacao + 1
+                numero_solicitacao += 1
                 lista = requests.get(f'https://api.auvo.com.br/v2/tasks/?paramFilter={data}&page={page}&pageSize={pageSize}&order={order}&selectfields={selectfields}', headers=headers)
                 if numero_solicitacao == 100:
                     break
-                page = page + 1 
+                page += 1
                 if lista.status_code == 200:
                     lista1 = json.loads(lista.content)
-                    
                     tarefa = 0
                     numero = 0
                     numero_ids = len(lista1['result']['entityList'])
                 else:
                     break
-                if numero_ids == 0:   
+                if numero_ids == 0:
                     print('')
                 else:
-                    print(f'esse é o numero de IDs {numero_ids}')
                     while True:
                         tasks = lista1['result']['entityList'][tarefa]
-                        tarefa = tarefa + 1 
+                        tarefa += 1
                         if tarefa != numero_ids:
-                            numero = numero + 1
+                            numero += 1
                             resposta = tasks
                             if resposta['attachments'] == []:
                                 user = resposta['userToName']
@@ -66,35 +91,43 @@ def baixar_anexos(data_inicio, data_final, pasta_destino):
                                         dt = datetime.strptime(data_tarefa, '%Y-%m-%dT%H:%M:%S')
                                         dia_mes = f'{dt.day:02d}-{dt.month:02d}'
                                         user = f"{dia_mes}-{user}-{numero_imagens}"
-                                        numero_imagens = numero_imagens + 1
+                                        numero_imagens += 1
                                         filename = f"{user}.png"
+                                        if imagem.endswith('.pdf'):
+                                            filename = f"{user}.pdf" 
                                         caminho_arquivo = os.path.join(pasta_destino, filename)
                                         response = requests.get(imagem)
-                                        response.raise_for_status() 
+                                        response.raise_for_status()
                                         with open(caminho_arquivo, "wb") as file:
                                             file.write(response.content)
                                             print(f"Anexo encontrado! {user} OK!")
                                     else:
-                                        break 
+                                        break
                         else:
-                            break     
+                            break
             else:
-                janela['-ELEMENTO_DA_INTERFACE-'].update('Novo valor')
-                page = page + 1
-                break      
-        sg.popup_ok('Todas as tarefas vistas😁')
+                page += 1
+                break
+        janela.write_event_value('-POPUP-', 'Todas as tarefas vistas 😁')
     except Exception as e:
-        sg.popup_error(f"ERRO:{str(e)} (me chame!)")
+        janela.write_event_value('-POPUP-', f"ERRO:{str(e)} (me chame!)")
+
+def baixar_anexos_thread(data_inicio, data_final, pasta_destino):
+    baixar_anexos(data_inicio, data_final, pasta_destino)
 
 while True:
     eventos, valores = janela.read()
     if eventos == sg.WINDOW_CLOSED or eventos == 'Sair':
         break
     elif eventos == '-BAIXAR-':
-        janela['-BAIXAR-'].update(disabled=True)  # Desabilita o botão 'Baixar'
+        janela['-BAIXAR-'].update(disabled=True)  
         data_inicio = valores['-DATA_INICIO-']
         data_final = valores['-DATA_FINAL-']
         pasta_destino = valores['-PASTA_DESTINO-']
-        baixar_anexos(data_inicio, data_final, pasta_destino)
-        janela['-BAIXAR-'].update(disabled=False)  # Habilita o botão 'Baixar'
+        thread = threading.Thread(target=baixar_anexos_thread, args=(data_inicio, data_final, pasta_destino))
+        thread.start()
+    elif eventos == '-POPUP-':
+        sg.popup_ok(valores['-POPUP-'])
+        janela['-BAIXAR-'].update(disabled=False)  
+
 janela.close()
